0bb26901f2ee8721e4f3a03ee6b53fb0e3bb92d7
mhamdibakr <elmhamdi.bakr@gmail.com>
2022-04-03 21:15:56 +0000

    last commit of the day

--- src/com/ps/Loops.java
+++ src/com/ps/Loops.java
@@ -100,13 +100,125 @@ public class Loops
         do
         {
             int res = dividend % divisor;
+            hcf = divisor;
+            dividend = divisor;
+            divisor = res;
 
-                hcf = divisor;
-                dividend = divisor;
-                divisor = res;
         }while (divisor != 0);
         System.out.println("The HCF of "+num1+" and "+num2+" is "+hcf);
     }
+
+    public static void HCF2(int num1 , int num2)
+    {
+        int hcf = 0;
+        for(int i = 1; i <= num1 ; i++)
+        {
+            for (int j =1; j <= i ; j++)
+            {
+                if(num1 % i == 0 && num2 % j == 0 && i == j)
+                {
+                    if(i > hcf)
+                        hcf = i;
+                }
+            }
+        }
+        System.out.println(hcf);
+    }
+
+    public static void twoNumbersSum()
+    {
+        Scanner scanner = new Scanner(System.in);
+        int number1, number2, sum;
+        char choice = ' ';
+        do
+        {
+            System.out.println("Enter the first number ");
+            number1 = scanner.nextInt();
+            System.out.println("Enter the second number ");
+            number2 = scanner.nextInt();
+            sum = number1 + number2;
+
+            System.out.println("The sum of "+number1+" and "+number2+" is "+sum);
+            System.out.println("Type c if you want to try againg and any key to leave");
+            choice = scanner.next().charAt(0);
+
+        }while (choice == 'c' || choice == 'C');
+    }
+
+    public static void pnzCount()
+    {
+        int currentNumber, countPos = 0, countNeg = 0 , countZeros = 0;
+        char choice = ' ';
+        Scanner scanner = new Scanner(System.in);
+        do
+        {
+            System.out.println("Enter a number");
+            currentNumber = scanner.nextInt();
+            if(currentNumber > 0)
+                countPos += 1;
+            else if (currentNumber < 0)
+                countNeg += 1;
+            else
+                countZeros += 1;
+
+            System.out.println("Type C if you want to continue or any other key if you want to leave");
+            choice = scanner.next().charAt(0);
+
+        }while (choice == 'c' || choice == 'C');
+        System.out.println("Positive count "+countPos+"\nNegative cout "+countNeg+"\nZeros count "+countZeros);
+    }
+
+    public static void smallestLargest()
+    {
+        Scanner scanner = new Scanner(System.in);
+        int number;
+        int largestNumber = Integer.MIN_VALUE;
+        int smallestNumber = Integer.MAX_VALUE;
+        char choice = ' ';
+        do
+        {
+            System.out.println("Enter a number");
+            number = scanner.nextInt();
+            if(number > largestNumber)
+                largestNumber = number;
+
+            if(number < smallestNumber)
+                smallestNumber = number;
+
+            System.out.println("Type a c if you want to continue or any other key to leave");
+            choice = scanner.next().charAt(0);
+        }while (choice == 'c' || choice == 'C');
+
+        System.out.println("Largest number is "+largestNumber+" Smallest number is "+smallestNumber);
+    }
+
+    public static int cubeN(int number)
+    {
+        int res = 1;
+        for (int i = 1 ; i <= 3 ; i++)
+        {
+            res *= number;
+        }
+        return res;
+    }
+    public static void armstrongNumber(int number)
+    {
+        int num = number, res = 0, flip;
+        do
+        {
+            flip = num % 10;
+            res += cubeN(flip);
+            num /= 10;
+
+        }while (num > 0);
+
+        if(res == number)
+            System.out.println("it's an armstrong number");
+        else
+            System.out.println("it's not an armstrong number");
+    }
+
+
     public static void prePost()
     {
         int i = 0 ;
--- src/com/ps/Main.java
+++ src/com/ps/Main.java
@@ -3,7 +3,7 @@ package com.ps;
 public class Main {
 
     public static void main(String[] args) {
-        Loops.HCF(90,60);
+        Loops.armstrongNumber(153);
 
 
     }
