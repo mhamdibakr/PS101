3dca40f70c79cfd30a4971ec1bbdffc49cbeb842
mhamdibakr <elmhamdi.bakr@gmail.com>
2022-04-18 16:29:39 +0000

    Just completing the 8th commit

--- src/com/ps/Main.java
+++ src/com/ps/Main.java
@@ -9,14 +9,6 @@ public class Main {
         String [] days = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday" , "Saturday"};
         int[] numArrays = {1 , 5 , 6 , 12 , 4 , 9};
         int[] array2 = {1,5,2};
-      //  Arrays.fillArray(numArrays,6);
-        Arrays.printArray(numArrays);
-        Arrays.sumArray(numArrays);
-        Arrays.avgArray(numArrays);
-        Arrays.largestAndSmallest(numArrays);
-        Arrays.sortArray(numArrays);
-        Arrays.searchArray(numArrays,5);
-        Arrays.mergeArrays(numArrays,array2);
 
     }
 }
