d65de095e921a654aaec2caff72a2979cf5c2c23
mhamdibakr <elmhamdi.bakr@gmail.com>
2022-04-18 09:54:13 +0000

    the array class ffs

--- src/com/ps/Main.java
+++ src/com/ps/Main.java
@@ -6,6 +6,11 @@ public class Main {
     {
         //Conditions.isPrimePrint(10);
         //Loops.isPalindrome(512215);
+        String [] days = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday" , "Saturday"};
+        for (String day :days)
+        {
+            System.out.println(day);
+        }
 
     }
 }
