8dbc151779e85cdd5bccdccc268fa5d7df4ccb78
mhamdibakr <elmhamdi.bakr@gmail.com>
2022-04-18 13:29:57 +0000

    merging two arrays

--- src/com/ps/Arrays.java
+++ src/com/ps/Arrays.java
@@ -118,4 +118,18 @@ public class Arrays
 
     }
 
+    public static void mergeArrays(int[] array1, int[] array2)
+    {
+        int[] array3 = new int[array1.length + array2.length];
+        for(int i = 0 ; i < array1.length ; i++)
+        {
+            array3[i] = array1[i];
+        }
+        for (int i = 0 ; i < array2.length; i++)
+        {
+            array3[array1.length+i]=array2[i];
+        }
+        printArray(array3);
+    }
+
 }
--- src/com/ps/Main.java
+++ src/com/ps/Main.java
@@ -8,6 +8,7 @@ public class Main {
         //Loops.isPalindrome(512215);
         String [] days = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday" , "Saturday"};
         int[] numArrays = {1 , 5 , 6 , 12 , 4 , 9};
+        int[] array2 = {1,5,2};
       //  Arrays.fillArray(numArrays,6);
         Arrays.printArray(numArrays);
         Arrays.sumArray(numArrays);
@@ -15,6 +16,7 @@ public class Main {
         Arrays.largestAndSmallest(numArrays);
         Arrays.sortArray(numArrays);
         Arrays.searchArray(numArrays,5);
+        Arrays.mergeArrays(numArrays,array2);
 
     }
 }
