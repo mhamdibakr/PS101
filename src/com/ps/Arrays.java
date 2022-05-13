dad30e6246ea7730fae6fbdb5579f92b8375cfbc
mhamdibakr <elmhamdi.bakr@gmail.com>
2022-05-13 12:54:42 +0100

    selection Sort

--- src/com/ps/Arrays.java
+++ src/com/ps/Arrays.java
@@ -144,6 +144,23 @@ public class Arrays
     }
 
     //selction sort
+    public static void selectionSort(int[] array)
+    {
+        int temp = '\0';
+        for(int i = 0 ; i < array.length - 1; i++)
+        {
+            for(int j = 0 ; j < array.length - i - 1 ; j++)
+            {
+                if(array[j] > array[j+1])
+                {
+                    temp = array[j];
+                    array[j]   = array[j+1];
+                    array[j+1] = temp;
+                }
+            }
+        }
+        printIntArray(array);
+    }
 
     // merge 2 arrays
 }
