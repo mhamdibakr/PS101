497a96b465afb368ee41897b68b46fff88c95779
mhamdibakr <elmhamdi.bakr@gmail.com>
2022-05-14 12:23:09 +0100

    getting the 2d array size

--- src/com/ps/Main.java
+++ src/com/ps/Main.java
@@ -11,6 +11,19 @@ public class Main {
 
         Arrays.print2dArray(array2);
         Arrays.ops2dArray(array2);
+
+
+        int[][] foo = new int[][] {
+                new int[] { 1, 2, 3 },
+                new int[] { 1, 2, 3, 4},
+        };
+
+
+        Arrays.print2dArray(foo);
+        System.out.println(foo.length); //2
+        System.out.println(foo[0].length); //3
+        System.out.println(foo[1].length); //4
+
     }
 }
 
