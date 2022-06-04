649c422714ba124b33d7ddb241a05587d903937b
mhamdibakr <elmhamdi.bakr@gmail.com>
2022-06-04 18:45:30 +0100

    twoNumberSum

--- src/com/ps/ArraysTwo.java
+++ src/com/ps/ArraysTwo.java
@@ -1,12 +1,60 @@
 package com.ps;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class ArraysTwo
 {
-    public static void ok(int n)
+    /*
+        write a function that takes in a non-empty array of distinct intigers
+        and a single integer representing a target sum. if any two numbers in the input
+        array sum up to the target sum, the function then should return them in an arraym in any order.
+        if no two numbers sum up to the target sum, the function should return an empty array.
+
+        note that the target sum has to be obtained by summing two diffrent integes in the array,
+        you can't add a single integer to itself in order to obtain the target sum.
+
+        you can assume that there will be at most one pair of numbers summing up the target sum.
+    */
+
+    public static void twoNumberSum(int[] array, int n)
     {
-        for(int i = 0 ; i <= n ; i++)
+        int[] res = new int[2];
+        if(array.length <= 1)
+            System.out.println("cant perform this op");
+        else
         {
-            System.out.print(i+" ");
+            for (int i = 0; i < array.length; i++)
+            {
+                for (int j = 0; j < array.length; j++)
+                {
+                    if (array[i] + array[j] == n && i != j)
+                    {
+                        res = new int[]{array[i], array[j]};
+                    }
+                }
+            }
         }
+        Arrays.printIntArray(res);
     }
+
+    public static int[] twoNumberSumvV2(int[] array, int n)
+    {
+        Set<Integer> nums = new HashSet<>();
+        for (int num : array)
+        {
+            int pn = n - num;
+            if(nums.contains(pn))
+            {
+                return new int[] {pn,num};
+            }
+            else
+            {
+                nums.add(num);
+            }
+        }
+        return new int[0];
+    }
+
+
 }
--- src/com/ps/Main.java
+++ src/com/ps/Main.java
@@ -7,12 +7,14 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        int[] array = {1, 23, 12};
+        int[] array = {3, 5, -4, 8, 11, 1, -1, 6};
         int[][] array2 = {{1, 2, 3}, {4, 5, 6}};
         int[][] array55 = {{7, 8}, {9, 10}, {11, 12}};
         String[] days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
 
-       ArraysTwo.ok(5);
+
+       int[] res = ArraysTwo.twoNumberSumvV2(array,10);
+       Arrays.printIntArray(res);
     }
 }
 
