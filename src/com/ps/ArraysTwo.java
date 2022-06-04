90fad8981c9f69e0f5a8cb3e727dd07d68a6d6e8
mhamdibakr <elmhamdi.bakr@gmail.com>
2022-06-04 16:30:36 +0100

    a new class for arrays

---
+++ src/com/ps/ArraysTwo.java
@@ -0,0 +1,12 @@
+package com.ps;
+
+public class ArraysTwo
+{
+    public static void ok(int n)
+    {
+        for(int i = 0 ; i <= n ; i++)
+        {
+            System.out.print(i+" ");
+        }
+    }
+}
